f46e1748505f4bfc03e0c8b3a65c5e7a6d7f2b91
Aparna <aparnababbar181@gmail.com>
2026-09-10 16:48:34 +0530

    Update house.py

--- house.py
+++ house.py
@@ -3,8 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import pickle
 
-df = pd.read_csv(
-    r'_YOUR_LOCATION_\house_data.csv')
+df = pd.read_csv('house_data.csv')
 
 columns = ['bedrooms', 'bathrooms', 'floors', 'yr_built', 'price']
 df = df[columns]
